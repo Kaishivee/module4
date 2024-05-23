78c3d47efc6f4bbb70c23fa4e3c8c974a1646de5
Дарья <167187299+Kaishivee@users.noreply.github.com>
2024-05-23 21:44:56 +0300

    Add files via upload

---
+++ Атрибуты и методы объекта.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 10
+
+    def current_floor(self):
+        for self.numberOfFloors in range(0, self.numberOfFloors +1):
+            print('Текущий этаж равен', house_.numberOfFloors)
+
+
+house_ = House()
+
+house_.current_floor()
