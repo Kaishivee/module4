6d61bceea6f6044338fbb42b5693b230305800a3
Дарья <167187299+Kaishivee@users.noreply.github.com>
2024-06-03 12:39:28 +0300

    Add files via upload

--- Наследование классов.py
+++ Наследование классов.py
@@ -5,19 +5,25 @@ class Car:
         self.name = name
 
     def __str__(self):
-         return 'Машина: {}, Модель: {}, Цена: {}, л.с: {}'.format(
-             self.__class__.__name__, self.name, self.price, self.horse_powers)
+         return 'Марка: {}, Модель: {}, Цена: {}, л.с: {}'.format(
+             self.__class__.__name__, self.name, self.price, self.horse_powers())
 
     def horse_powers(self):
         return horse_powers
 
 class Nissan(Car):
     price = 830000
-    horse_powers = 143
+
+    def horse_powers(self):
+        horse_powers = 143
+        return horse_powers
 
 class Kia(Car):
     price = 2600000
-    horse_powers = 150
+
+    def horse_powers(self):
+        horse_powers = 150
+        return horse_powers
 
 nissan_terrano = Nissan(name='Nissan Terrano')
 kia_rio = Kia(name='Kia Rio')
